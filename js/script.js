7698e9baaaddae7f4a3c2772cf6c5955617eb835
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-18 20:27:40 +0600

    mathematical function and get value form input field function created

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 document.getElementById('blog-btn').addEventListener('click', function(){
     window.location.href = "../blog.html";
 })
+
+// These two functions will do the mathematical calculation
+const multiplyTwoValues = function(value1, value2){
+    return value1 * value2;
+}
+const multiplyTwoValuesWithHalf = function(value1, value2){
+    return 0.5 * value1 * value2;
+}
+
+// It'll take an ID name of an input Field as parameter and return it's value as floating Number
+const getValueFromInputField = function(elementId){
+    const inputField = document.getElementById(elementId);
+    const valueString = inputField.value;
+    const value = parseFloat(valueString);
+    return value;
+}
