0badad15be4f17d46d2851ba8ff85b728b3a25c1
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-18 23:10:54 +0600

    Random background color on card hover added

---
+++ js/randomBackgroundColor.js
@@ -0,0 +1,89 @@
+const card1 = document.getElementById('card-1');
+
+card1.addEventListener('mouseover', function() {
+  card1.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const card2 = document.getElementById('card-2');
+
+card2.addEventListener('mouseover', function() {
+  card2.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const card3 = document.getElementById('card-3');
+
+card3.addEventListener('mouseover', function() {
+  card3.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const card4 = document.getElementById('card-4');
+
+card4.addEventListener('mouseover', function() {
+  card4.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const card5 = document.getElementById('card-5');
+
+card5.addEventListener('mouseover', function() {
+  card5.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const card6 = document.getElementById('card-6');
+
+card6.addEventListener('mouseover', function() {
+  card6.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
